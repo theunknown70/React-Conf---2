db2ff6c59d6f6f3bb8772b49f3cf36105da5f9c2
Sumeet Wagh <sumeetwagh70@gmail.com>
2022-07-03 23:20:05 +0530

    final

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -21,7 +21,7 @@ function RenderLeader({leader}) {
 
 }
 
-function LeaderList(props) {                    
+function LeaderList(props) {                                // list
 
     const leaders = props.leaders.leaders.map((leader) => {
         return (
